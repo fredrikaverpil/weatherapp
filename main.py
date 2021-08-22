5eca8a9b9680822e2d75f79f1774fe7d6ba9af7d
Fredrik Averpil <fredrik@averpil.com>
2021-08-22 11:31:32 +0200

    Combine error with logging

--- main.py
+++ main.py
@@ -12,22 +12,26 @@ from services import openweather_service
 from views import home
 
 
+class APIKeyError(Exception):
+    def __init__(self, message: str):
+        logger.error(message)
+        super().__init__(message)
+
+
 app = fastapi.FastAPI()
 
 init_logging()
 
 
-def configure_api_keys():
-    file = Path("settings.json").absolute()
-    if not file.exists():
-        logger.warning(
-            f"WARNING: {file} file not found, you cannot continue, please see settings_template.json"
-        )
-        raise Exception(
-            "settings.json file not found, you cannot continue, please see settings_template.json"
+def configure_api_key():
+    key_file = Path("settings.json").absolute()
+    if not key_file.exists():
+        raise APIKeyError(
+            f"{key_file} file not found, you cannot continue, "
+            "please see settings_template.json"
         )
 
-    with open("settings.json") as fin:
+    with open(key_file) as fin:
         settings = json.load(fin)
         openweather_service.api_key = settings.get("api_key")
 
@@ -40,7 +44,7 @@ def configure_routing():
 
 def configure():
     configure_routing()
-    configure_api_keys()
+    configure_api_key()
 
 
 if __name__ == "__main__":
